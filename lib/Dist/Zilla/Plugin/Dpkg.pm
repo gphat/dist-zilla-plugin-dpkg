9cb33320cff5c85ae77a81b847a87fc93dc38661
Cory G Watson <gphat@cpan.org>
2011-12-19 16:01:18 -0600

    Docs, _default bits and looping over file names rather than a block of code per.

--- lib/Dist/Zilla/Plugin/Dpkg.pm
+++ lib/Dist/Zilla/Plugin/Dpkg.pm
@@ -1,7 +1,6 @@
 package Dist::Zilla::Plugin::Dpkg;
 use Moose;
 
-# Skipped:
 # * copyright
 # * docs
 # * files
@@ -17,6 +16,26 @@ use Moose;
 # * templates
 # * watch
 
+# ABSTRACT: Generate Dpkg files for your perl module
+
+=head1 SYNOPSIS
+
+  [Dpkg]
+  architecture = amd64
+  default_template = package/debian/default
+
+=head1 DESCRIPTION
+
+Dist::Zilla::Plugin::Dpkg generates Debian' controls files that you can use
+with debhelper to generate packages of your perl module.
+
+There are a handful of tools that provide similar functionality, but most of
+them expect your perl module to have a standard installation mechanism.  This
+module was born of a need for customization, as it used per-package perlbrews
+and all manner of custom bits.
+
+=cut
+
 use Dist::Zilla::File::InMemory;
 use Text::Template;
 
@@ -24,10 +43,10 @@ with 'Dist::Zilla::Role::InstallTool';
 
 =begin :prelude
 
-=head2 Templates
+=head1 TEMPLATES
 
 This plugin uses L<Text::Template>.  The following variables will be passed
-to any templates that are processed, using attributes as values.
+to any templates that are processed, using attributes as values:
 
 =over 4
 
@@ -51,6 +70,17 @@ to any templates that are processed, using attributes as values.
 
 =back
 
+=head1 SUBCLASSING
+
+Each of the aforementioned template methods has an accompanying method that
+provides a default template.  Most of these are undefined and therefore
+unused.  This subclassing behavior allows you to create subclasses of
+Dist::Zilla::Plugin::Dpkg that provide default templates for many of the files.
+
+The idea is to allow the easy creation of something like a
+Dist::Zilla::Plugin::Dpkg::Starman that provides boilerplate code for a
+L<Starman>-based application.
+
 =end :prelude
 
 =attr architecture
@@ -65,21 +95,43 @@ has 'architecture' => (
     default => 'any'
 );
 
-=attr compat
+=attr compat_template
 
-Contents of the C<compat> file. Defaults to 7.
+If set, the specified file is used as a template for the C<compat> file.
+
+=method has_compat_template
 
 =cut
 
-has 'compat' => (
+has 'compat_template' => (
     is => 'ro',
     isa => 'Str',
-    default => 7
+    predicate => 'has_compat_template'
+);
+
+=attr compat_template_default
+
+A default compat file template that will be used it a template isn't provided
+to C<compat_template>.
+
+=method has_compat_template_default
+
+=cut
+
+has 'compat_template_default' => (
+    is => 'ro',
+    isa => 'Str',
+    predicate => 'has_compat_template_default',
+    default => "7\n"
 );
 
 =attr conffiles_template
 
 If set, the specified file is used as a template for the C<conffiles> file.
+
+=method has_conffiles_template
+
+Predicate that is true if there is a conffiles_template
 
 =cut
 
@@ -89,9 +141,26 @@ has 'conffiles_template' => (
     predicate => 'has_conffiles_template'
 );
 
+=attr conffiles_template_default
+
+A default conffiles file template that will be used it a template isn't
+provided to C<conffiles_template>.
+
+=method has_conffiles_template_template
+
+=cut
+
+has 'conffiles_template_default' => (
+    is => 'ro',
+    isa => 'Str',
+    predicate => 'has_conffiles_template_default'
+);
+
 =attr config_template
 
 If set, the specified file is used as a template for the C<config> file.
+
+=method has_config_template
 
 =cut
 
@@ -101,10 +170,27 @@ has 'config_template' => (
     predicate => 'has_config_template'
 );
 
+=attr config_template_default
+
+A default config file template that will be used it a template isn't provided
+to C<config_template>.
+
+=method has_config_template_default
+
+=cut
+
+has 'config_template_default' => (
+    is => 'ro',
+    isa => 'Str',
+    predicate => 'has_config_template_default',
+);
+
 =attr control_template
 
 If set, the specified file is used as a template for the C<control> file.
 If not set uses an internal default.
+
+=method has_control_template
 
 =cut
 
@@ -114,9 +200,38 @@ has 'control_template' => (
     predicate => 'has_control_template'
 );
 
+=attr control_template_default
+
+A default control file template that will be used it a template isn't provided
+to C<control_template>.
+
+=method has_control_template_default
+
+=cut
+
+has 'control_template_default' => (
+    is => 'ro',
+    isa => 'Str',
+    predicate => 'has_control_template_default',
+    default => 'Source: {$package_name}
+Section: {$package_section}
+Priority: {$package_priority}
+Maintainer: {$author}
+Build-Depends: {$package_depends}
+Standards-Version: 3.8.4
+
+Package: {$package_name}
+Architecture: {$architecture}
+Depends: {$package_binary_depends}
+Description: {$package_description}
+'
+);
+
 =attr default_template
 
 If set, the specified file is used as a template for the C<default> file.
+
+=method has_default_template
 
 =cut
 
@@ -126,9 +241,26 @@ has 'default_template' => (
     predicate => 'has_default_template'
 );
 
+=attr default_template_default
+
+A default default file template that will be used it a template isn't provided
+to C<default_template>.
+
+=method has_default_template_default
+
+=cut
+
+has 'default_template_default' => (
+    is => 'ro',
+    isa => 'Str',
+    predicate => 'has_default_template_default'
+);
+
 =attr init_template
 
 If set, the specified file is used as a template for the C<init> file.
+
+=method has_init_template
 
 =cut
 
@@ -138,9 +270,26 @@ has 'init_template' => (
     predicate => 'has_init_template'
 );
 
+=attr init_template_default
+
+A default init file template that will be used it a template isn't provided
+to C<init_template>.
+
+=method has_init_template_default
+
+=cut
+
+has 'init_template_default' => (
+    is => 'ro',
+    isa => 'Str',
+    predicate => 'has_init_template_default'
+);
+
 =attr install_template
 
 If set, the specified file is used as a template for the C<install> file.
+
+=method has_install_template
 
 =cut
 
@@ -148,6 +297,21 @@ has 'install_template' => (
     is => 'ro',
     isa => 'Str',
     predicate => 'has_install_template'
+);
+
+=attr install_template_default
+
+A default install file template that will be used it a template isn't provided
+to C<install_template>.
+
+=method has_install_template_default
+
+=cut
+
+has 'install_template_default' => (
+    is => 'ro',
+    isa => 'Str',
+    predicate => 'has_install_template_default'
 );
 
 =attr package_depends
@@ -246,6 +410,8 @@ has 'package_section' => (
 
 If set, the specified file is used as a template for the C<postinst> file.
 
+=method has_postinst_template
+
 =cut
 
 has 'postinst_template' => (
@@ -254,9 +420,26 @@ has 'postinst_template' => (
     predicate => 'has_postinst_template'
 );
 
+=attr postinst_template_default
+
+A default postinst file template that will be used it a template isn't provided
+to C<postinst_template>.
+
+=method has_postinst_template_default
+
+=cut
+
+has 'postinst_template_default' => (
+    is => 'ro',
+    isa => 'Str',
+    predicate => 'has_postinst_template_default'
+);
+
 =attr postrm_template
 
 If set, the specified file is used as a template for the C<postrm> file.
+
+=method has_postrm_template
 
 =cut
 
@@ -266,9 +449,26 @@ has 'postrm_template' => (
     predicate => 'has_postrm_template'
 );
 
+=attr postrm_template_default
+
+A default postrm file template that will be used it a template isn't provided
+to C<postrm_template>.
+
+=method has_postrm_template_default
+
+=cut
+
+has 'postrm_template_default' => (
+    is => 'ro',
+    isa => 'Str',
+    predicate => 'has_postrm_template_default'
+);
+
 =attr rules_template
 
 If set, the specified file is used as a template for the C<rules> file.
+
+=method has_rules_template
 
 =cut
 
@@ -278,33 +478,27 @@ has 'rules_template' => (
     predicate => 'has_rules_template'
 );
 
-has '_control_file' => (
+=attr rules_template_default
+
+A default rules file template that will be used it a template isn't provided
+to C<rules_template>.
+
+=method has_rules_template_default
+
+=cut
+
+has 'rules_template_default' => (
     is => 'ro',
     isa => 'Str',
-    default => 'Source: {$package_name}
-Section: {$package_section}
-Priority: {$package_priority}
-Maintainer: {$author}
-Build-Depends: {$package_depends}
-Standards-Version: 3.8.4
-
-Package: {$package_name}
-Architecture: {$architecture}
-Depends: {$package_binary_depends}
-Description: {$package_description}
-'
+    predicate => 'has_rules_template_default'
 );
 
 sub setup_installer {
     my ($self, $arg) = @_;
 
-    # Compat file
-    my $compat = Dist::Zilla::File::InMemory->new({
-        content => $self->compat."\n",
-        name => 'debian/compat'
-    });
-    $self->add_file($compat);
-    
+    my @req_files = qw(compat control default install postinst postrm);
+    my @opt_files = qw(conffiles config init rules);
+
     # Now for the templates
     my %vars = (
         architecture    => $self->architecture,
@@ -319,108 +513,45 @@ sub setup_installer {
         version         => $self->zilla->version
     );
     
-    # conffiles file
-    if($self->has_conffiles_template) {
-        die "Can't find file: ".$self->conffiles_template unless -e $self->conffiles_template;
-        my $template = Text::Template->new(TYPE => 'FILE', SOURCE => $self->conffiles_template);
-        $self->add_file(Dist::Zilla::File::InMemory->new({
-            content => $template->fill_in(HASH => \%vars),
-            name => 'debian/conffiles'
-        }));
+    foreach my $file (@req_files) {
+        $self->_generate_file($file, 1, \%vars);
     }
-
-    # config file
-    if($self->has_config_template) {
-        die "Can't find file: ".$self->config_template unless -e $self->config_template;
-        my $template = Text::Template->new(TYPE => 'FILE', SOURCE => $self->config_template);
-
-        $self->add_file(Dist::Zilla::File::InMemory->new({
-            content => $template->fill_in(HASH => \%vars),
-            name => 'debian/config'
-        }));
+    foreach my $file (@opt_files) {
+        $self->_generate_file($file, 0, \%vars);
     }
+}
 
-    # control file
-    if($self->has_control_template) {
-        die "Can't find file: ".$self->control_template unless -e $self->control_template;
-        my $template = Text::Template->new(TYPE => 'FILE', SOURCE => $self->control_template);
+sub _generate_file {
+    my ($self, $file, $required, $vars) = @_;
 
-        $self->add_file(Dist::Zilla::File::InMemory->new({
-            content => $template->fill_in(HASH => \%vars),
-            name => 'debian/control'
-        }));
+    my $pred = 'has_'.$file.'_template';
+    my $temp = $file.'_template';
+    my $pred_def = 'has_'.$file.'_template_default';
+    my $def = $file.'_template_default';
+
+    my $template;
+    if($self->$pred) {
+        # We have a template, use it.
+        die "Can't find file: ".$self->$temp unless -e $self->$temp;
+        $template = Text::Template->new(TYPE => 'FILE', SOURCE => $self->$temp);
+        $self->log("Used template for file '$file'");
+    } elsif($self->$pred_def) {
+        # We have a default, use it
+        $template = Text::Template->new(TYPE => 'STRING', SOURCE => $self->$def);
+        $self->log("Used default for file '$file'");
     } else {
-        my $template = Text::Template->new(TYPE => 'STRING', SOURCE => $self->_control_file);
-
-        $self->add_file(Dist::Zilla::File::InMemory->new({
-            content => $template->fill_in(HASH => \%vars),
-            name => 'debian/control'
-        }));
+        # Blow up, we ain't got shit
+        $self->log("No template or default for '$file'");
+        if($required) {
+            die "No template or default provided for '$file'";
+        }
     }
 
-    # default file
-    if($self->has_default_template) {
-        die "Can't find file: ".$self->default_template unless -e $self->default_template;
-        my $template = Text::Template->new(TYPE => 'FILE', SOURCE => $self->default_template);
-
+    if(defined($template)) {
+        $self->log("Added file for '$file'");
         $self->add_file(Dist::Zilla::File::InMemory->new({
-            content => $template->fill_in(HASH => \%vars),
-            name => 'debian/default'
-        }));
-    }
-
-    # init file
-    if($self->has_init_template) {
-        die "Can't find file: ".$self->init_template unless -e $self->init_template;
-        my $template = Text::Template->new(TYPE => 'FILE', SOURCE => $self->init_template);
-
-        $self->add_file(Dist::Zilla::File::InMemory->new({
-            content => $template->fill_in(HASH => \%vars),
-            name => 'debian/init'
-        }));
-    }
-
-    # install file
-    if($self->has_install_template) {
-        die "Can't find file: ".$self->install_template unless -e $self->install_template;
-        my $template = Text::Template->new(TYPE => 'FILE', SOURCE => $self->install_template);
-
-        $self->add_file(Dist::Zilla::File::InMemory->new({
-            content => $template->fill_in(HASH => \%vars),
-            name => 'debian/install'
-        }));
-    }
-
-    # postinst file
-    if($self->has_postinst_template) {
-        die "Can't find file: ".$self->postinst_template unless -e $self->postinst_template;
-        my $template = Text::Template->new(TYPE => 'FILE', SOURCE => $self->postinst_template);
-
-        $self->add_file(Dist::Zilla::File::InMemory->new({
-            content => $template->fill_in(HASH => \%vars),
-            name => 'debian/postinst'
-        }));
-    }
-
-    # postrm file
-    if($self->has_postrm_template) {
-        die "Can't find file: ".$self->postrm_template unless -e $self->postrm_template;
-        my $template = Text::Template->new(TYPE => 'FILE', SOURCE => $self->postrm_template);
-
-        $self->add_file(Dist::Zilla::File::InMemory->new({
-            content => $template->fill_in(HASH => \%vars),
-            name => 'debian/postrm'
-        }));
-    }
-
-    # rules file
-    if($self->has_rules_template) {
-        die "Can't find file: ".$self->rules_template unless -e $self->rules_template;
-        my $template = Text::Template->new(TYPE => 'FILE', SOURCE => $self->rules_template);
-
-        $self->add_file(Dist::Zilla::File::InMemory->new({
-            content => $template->fill_in(HASH => \%vars),
-            name => 'debian/rules'
+            content => $template->fill_in(HASH => $vars),
+            name => "debian/$file"
         }));
     }
 }
